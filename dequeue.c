2db0428e7e3f993f43d076ccfdccdf4fd28eae3e
SHREYAS LALGE <lalgeshreyas@gmail.com>
2024-05-13 11:34:18 +0530

    Update dequeue.c

--- dequeue.c
+++ dequeue.c
@@ -63,6 +63,55 @@ i=(i+1)%n;
 printf("%c",dq[r]);
 }
 }
+void dqfront()
+{
+if(f==-1 && r==-1)
+{
+printf("KHAALI HAI RE BABA");
+}
+else if(f==r)
+{
+printf("deleted element %c",dq[f]);
+f=-1;
+r=-1;
+
+}
+else if(f==n-1)
+{
+    printf("deleted element %c ",dq[f]);
+    f=0;
+
+}
+else{
+    printf("DELETED ELEMENT %c",dq[f]);
+    f++;
+    
+}
+}
+
+void dqrear()
+{
+if(f==-1 && r==-1)
+{
+printf("queue khaali hai ");
+}
+else if(f==r)
+{
+printf("removed elemtn %c",dq[r]);
+r=f=-1;
+}
+else if(r==0)
+{
+printf("deleted element %c ",dq[r]);
+r=n-1;
+
+}
+else
+{
+    printf("DELETED ELEMENT %c",dq[r]);
+    r--;
+}
+}
 main()
 {
     char dku;
